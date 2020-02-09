261d507c49e6f25a4f2f899a1f4f7d532aa9ea08
Paulo Gabriel <pgj1085@gmail.com>
2020-02-09 17:21:58 -0300

    Bugs com ponto corridos

--- js/controller/calcController.js
+++ js/controller/calcController.js
@@ -133,7 +133,7 @@ class CalcController {
                 this.pushOperation(value);
             else {
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation(newValue);
                 this.setLastNumberToDisplay();
             }
     }
@@ -141,6 +141,9 @@ class CalcController {
     addDot(){
 
         let lastOperation = this.getLastOperation();
+
+        if(typeof lastOperation == "string" && lastOperation.split("").indexOf(".") > -1)
+            return 0;
 
         if(this.isOperator(lastOperation) || !lastOperation){
             this.pushOperation("0.");
